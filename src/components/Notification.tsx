f5a14d9c9a75d8f813172c9a3e821d93f2211f71
vanntile <33692650+vanntile@users.noreply.github.com>
2022-01-10 18:45:25 +0200

    wip(uiux): :construction: add temporary notification styling

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -5,6 +5,14 @@ interface Props {
 }
 
 const Notification: React.FC<Props> = ({ notification }) =>
-  notification ? <div className={`notification ${notification.type}`}>{notification.message}</div> : null
+  notification ? (
+    <div
+      className={`text-lg py-3 px-2 rounded mb-3 border-2 border-solid ${
+        notification.type === 'error' ? 'bg-red-200 border-red-600' : 'bg-blue-200 border-blue-600'
+      }`}
+    >
+      {notification.message}
+    </div>
+  ) : null
 
 export default Notification
